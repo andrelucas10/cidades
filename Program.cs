8a35d6c34cf2af54342bfed5cba1890be70bac2a
André e Guilherme <andrelviana6@gmail.com>
2025-09-19 22:37:43 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
